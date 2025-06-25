674578efb87b6f488cdf3c26e5979893932f4192
Kaya <95276965+kytpbs@users.noreply.github.com>
2025-06-25 21:18:29 +0200

    add array format toggling
    Tbh this should be automatically changed and enforced by a formatter but
    in projects with formatters that don't enforce it or without formatters,
    this is a very usefull plugin

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -8,4 +8,13 @@ return {
         'numToStr/Comment.nvim',
         lazy = false
     },
+
+    {
+        "Wansmer/treesj",
+        keys = {
+            { "<leader>j", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
+        },
+        opts = { use_default_keymaps = false, max_join_length = 150 },
+    },
+
 }
