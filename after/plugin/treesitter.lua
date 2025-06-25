1be4ec1dd2aaa3579a7203181af09da87ae012e9
Kaya <95276965+kytpbs@users.noreply.github.com>
2025-06-25 20:48:45 +0200

    Fix Telescope

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,26 @@
-require'nvim-treesitter'.setup {
-  -- Directory to install parsers and queries to
-  install_dir = vim.fn.stdpath('data') .. '/site'
-}
+-- previous setup, for main branch
+--
+-- require'nvim-treesitter'.setup {
+--   -- Directory to install parsers and queries to
+--   install_dir = vim.fn.stdpath('data') .. '/site'
+-- }
+--
+-- require'nvim-treesitter'.install ({ 'help', 'lua', 'json', 'python', 'java'})
 
-require'nvim-treesitter'.install ({ 'help', 'lua', 'json', 'python'})
+-- current temp setup for master 0.10 branch, only for telescope for now
+local configs = require("nvim-treesitter.configs")
+
+configs.setup({
+	ensure_installed = {
+        "lua",
+		"python",
+        "java",
+        "powershell",
+		"cpp",
+        "dockerfile",
+		"javascript",
+	},
+	sync_install = false,
+	highlight = { enable = true },
+	indent = { enable = false },
+})
