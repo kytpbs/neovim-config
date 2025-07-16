0e2a6928889819f33751e32a64b2c63d04a09f6c
Kaya <95276965+kytpbs@users.noreply.github.com>
2025-07-16 16:19:10 +0200

    Update telescope to Master to fix treesitter
    It seems there has been no new relases for telescope even though it has
    seen a lot of updates in the master branch, and now there is a mismatch
    for the treesitter plugin that the relase version expects and the
    current treesitter main.
    
    ## 2 solutions exist to fix this
    
    ### Set treesitter back to master
    Fixes the solution but other newer plugins that are expecting the new
    treesitter functions don't work correctly
    
    ### Pin Telescope to master branch instead of 0.1.x
    This commits solution, which pins it telescope to master
    
    Solves all issues but may fuck up randomly

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,26 +1,9 @@
 -- previous setup, for main branch
---
--- require'nvim-treesitter'.setup {
---   -- Directory to install parsers and queries to
---   install_dir = vim.fn.stdpath('data') .. '/site'
--- }
---
--- require'nvim-treesitter'.install ({ 'help', 'lua', 'json', 'python', 'java'})
 
--- current temp setup for master 0.10 branch, only for telescope for now
-local configs = require("nvim-treesitter.configs")
+require 'nvim-treesitter'.setup {
+    -- Directory to install parsers and queries to
+    install_dir = vim.fn.stdpath('data') .. '/site'
+}
 
-configs.setup({
-	ensure_installed = {
-        "lua",
-		"python",
-        "java",
-        "powershell",
-		"cpp",
-        "dockerfile",
-		"javascript",
-	},
-	sync_install = false,
-	highlight = { enable = true },
-	indent = { enable = false },
-})
+require 'nvim-treesitter'.install({ 'help', 'vim', 'lua', 'json', 'python', 'java', 'powershell', 'cpp', 'dockerfile',
+    'javascript' })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,9 @@
 return {
 	{
 		'nvim-treesitter/nvim-treesitter',
-        tag = "v0.10.0",
+        -- tag = "v0.10.0",
 		lazy = false,
-		-- branch = 'main',
+		branch = 'main',
 		build = ':TSUpdate',
 		priority = 1000, --high-priority since other plugins like treesitter need it
 	},
