784024987057eb082d1b38140c4bb960ada1cc2d
Kaya <95276965+kytpbs@users.noreply.github.com>
2025-07-16 14:41:17 +0200

    Fully add Primagen Remap
    From 0 to LSP video

--- lua/main/remap.lua
+++ lua/main/remap.lua
@@ -1,3 +1,4 @@
+-- The Primagen code
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "gb", "<C-o>")
@@ -9,6 +10,8 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
