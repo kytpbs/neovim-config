b04bed5fa8ad0988f7586fca2719f3e4eab49403
Kaya <kytpbs@gmail.com>
2026-05-05 01:44:09 +0200

    make it use pwsh only on windows

--- lua/main/set.lua
+++ lua/main/set.lua
@@ -5,8 +5,12 @@ vim.opt.signcolumn = 'yes'
 
 vim.cmd([[filetype plugin indent on]])
 
-vim.o.shell = "pwsh.exe"
-vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command $PSStyle.OutputRendering = 'PlainText';"
+-- use pwsh on windows
+if vim.fn.has('win32') == 1 then
+    vim.o.shell = "pwsh.exe"
+    vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command $PSStyle.OutputRendering = 'PlainText';"
+end
+
 vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
 vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
 vim.o.shellquote = ""
